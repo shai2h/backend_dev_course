6ad2a0754db30e38ba0d45e2f40e0419455ad287
Shaydullin Ilfat <sweden.fan@yandex.ru>
2025-11-17 18:38:30 +0300

    begin fastapi

---
+++ 02_fastapi_begin/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def func():
+    return 'Hello World!'
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True, log_level='info')
