20e39c3bbbaa528aaa1117911a980e87b2d3d987
Shaydullin Ilfat <sweden.fan@yandex.ru>
2025-11-18 22:24:22 +0300

    fastapi/routes

---
+++ 02_fastapi_begin/routes/hotels.py
@@ -0,0 +1,73 @@
+from fastapi import Query, Body, APIRouter, HTTPException
+
+router = APIRouter()
+
+hotels = [
+    {"id": 1, "title": "Сочи", 'name': "sochi"},
+    {"id": 2, "title": "Дубай", 'name': "dubai"},
+    {"id": 3, "title": "Москва", 'name': "moskva"},
+    {"id": 4, "title": "Казань", 'name': "kazan"},
+]
+
+@router.get('/hotels', summary="Получить список отелей", description='Дополнительная информация')
+def get_hotels(
+    title: str | None = Query(None, description='Название отеля')
+):
+    if title:
+        return [hotel for hotel in hotels if hotel["title"].lower() == title.lower()]
+    else:
+        return hotels
+    
+
+@router.delete('/delete_holel/{hotel_id}', summary='Удалить отель', description='Дополнительная информация')
+def delete_hotel(
+    hotel_id: int
+):
+    global hotels
+    before_len = len(hotels)
+    hotels = [hotel for hotel in hotels if hotel['id'] != hotel_id]
+    if len(hotels) == before_len:
+        raise(HTTPException(status_code=404, detail='Отель не найден'))
+    
+    return {
+        'message':'deleted',
+        }
+
+
+@router.post('/add_hotel', summary='Добавить отель', description='Дополнительная информация')
+def add_hotel(
+    title: str = Body(embed=True), #embed -- возвращает пример ключ:значение & Body - для передачи в теле
+):
+    hotels.append({'id': len(hotels) + 1, 'title': title})
+    return {
+        'status': 'OK',
+        'message': f'{title} - added'
+    }
+
+
+@router.patch('/edit_hotel/{hotel_id}', summary='Частичное обновление данных', description='Дополнительная информация')
+def edit_hotel(
+    hotel_id: int,
+    title: str | None = Body(None, embed=True),
+    name: str | None = Body(None, embed=True)
+):
+    global hotels
+    for hotel in hotels:
+        if hotel['id'] == hotel_id:
+            if title is not None:
+                hotel['title'] = title
+            if name is not None:
+                hotel['name'] = name
+    return {'status': 'ok'}
+
+
+@router.put('/put_hotel/{hotel_id}', summary='Полное обновление данных')
+def edit_hotel(
+    hotel_id: int,
+    title: str = Body(embed=True),
+    name: str = Body(embed=True)
+):
+    for hotel in hotels:
+        if hotel['id'] == hotel_id:
+            hotel['title'], hotel['name'] = title, name
+    return {'status': 'ok'}
