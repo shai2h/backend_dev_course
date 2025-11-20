52111787e5f7f7f08682764c9786afe04afae8d5
Shaydullin Ilfat <sweden.fan@yandex.ru>
2025-11-20 20:58:14 +0300

    main for pydantic dir

---
+++ 02_fastapi_begin/pydantic/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+from hotels import router as hotel_router
+
+
+
+app = FastAPI()
+
+
+app.include_router(hotel_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
