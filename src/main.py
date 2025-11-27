f6a0f131b7321c1a0d0b7058983a1dfb3ae9d4fd
Shaydullin Ilfat <sweden.fan@yandex.ru>
2025-11-27 20:38:06 +0300

    created absolute path

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import uvicorn
+
+# Добавляем корневую директорию проекта в sys.path, чтобы можно было использовать абсолютные импорты
+# Это позволяет импортировать модули из src и других пакетов, несмотря на расположение запускаемого файла
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as hotel_router
+
+
+
+app = FastAPI()
+
+
+app.include_router(hotel_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
