bb5efccb8456c34a796c0f35b5d5f838c247c481
Shaydullin Ilfat <sweden.fan@yandex.ru>
2025-12-01 23:04:34 +0300

    added db connect

---
+++ src/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy import text
+import asyncio
+from src.config import settings
+
+engine = create_async_engine(settings.DB_URL)
+
+async def func():
+    async with engine.begin() as conn:
+        res = await conn.execute(text("SELECT version()"))
+        print(res.fetchone())
+
+
+asyncio.run(func())
