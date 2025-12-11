52b985f32242d35ee156d1890f7bd1c0052250db
Shaydullin Ilfat <sweden.fan@yandex.ru>
2025-12-11 22:30:41 +0300

    added sys.path

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
