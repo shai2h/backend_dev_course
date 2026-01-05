613b605346379066d0fe8c0b5b38fda5bbd8bf99
Shaydullin Ilfat <sweden.fan@yandex.ru>
2026-01-05 18:27:22 +0300

    added get_all() to HotelRepository

--- repositories/hotels.py
+++ repositories/hotels.py
@@ -1,6 +1,31 @@
+from sqlalchemy import select, func
+
 from repositories.base import BaseRepository
 from src.models.hotels import HotelOrm
 
 
 class HotelRepository(BaseRepository):
     model = HotelOrm
+
+    async def get_all(
+            self,
+            location,
+            title,
+            limit,
+            offset,
+        ):
+        query = select(HotelOrm)
+        if title:
+            query = query.filter(func.lower(HotelOrm.title).contains(title.strip().lower()))
+        if location:
+            query = query.filter(func.lower(HotelOrm.location).like(location.strip().lower()))
+        query = (
+            query
+            .limit(limit)
+            .offset(offset)
+        )
+        # логируем запрос к БД
+        print(query.compile(compile_kwargs={"literal_binds": True}))
+
+        result = await self.session.execute(query)
+        return result.scalars().all()
